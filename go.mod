f4231169a0394da8338e9b72196af5b1337345ad
Benjamin Grandfond <benjamin.grandfond@vestiairecollective.com>
2019-07-03 01:56:02 +0200

    Add database connection

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/benja-M-1/thegoodcorner
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.1.1
+	github.com/stretchr/testify v1.3.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"github.com/benja-M-1/thegoodcorner/handlers"
+	"github.com/benja-M-1/thegoodcorner/models"
 	"log"
 	"net/http"
 )
 
 func main() {
+	err := models.DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/fizzbuzz", handlers.FizzbuzzHandler)
 
 	log.Fatal(http.ListenAndServe(":80", nil))
+
+	defer models.DB.Close()
 }
